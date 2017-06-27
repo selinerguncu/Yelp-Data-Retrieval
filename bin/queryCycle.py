b324723afff7373d1cf121169c2b66763a7c33e1
Selin Erguncu <erguncu@usc.edu>
2017-06-26 22:35:56 -0700

    mainland coordinates adjusted
    - covering only the bay area

--- bin/queryCycle.py
+++ bin/queryCycle.py
@@ -17,12 +17,16 @@ global json_contents
 
 # appPath = os.getcwd()
 
-# categories = ["restaurants", "bars", "nightlife", "coffee", "beautysvc", "food", "giftshops"]
-categories = ["restaurants", "bars", "nightlife", "coffee"]
-prices = ["1", "2", "3", "4"]
+coordinates = areaCoordinates.area
 
-LATT_INCREMENT = 0.015
-LONG_INCREMENT = 0.015
+categories = ["restaurants", "bars", "nightlife", "coffee", "beautysvc", "food", "giftshops"]
+# categories = ["restaurants", "bars", "nightlife", "coffee", "beautysvc", "food"]
+prices = ["1", "2", "3", "4"]
+# categories = ["restaurants"]
+# prices = ["1"]
+
+# LATT_INCREMENT = 0.005
+# LONG_INCREMENT = 0.005
 
 #mainland coordinates:
 LATT_WEST = 44.81314
@@ -35,45 +39,76 @@ LONG_EAST = -81.08813
 LONG_NORTH = -95.0499998
 LONG_SOUTH = -124.73246
 
-#sample area in SF coordinates:
-# LATT_WEST = 37.7807
-# LATT_EAST = 37.75499
-# LATT_NORTH = 37.78835
-# LATT_SOUTH = 37.74413
+def checkIfValidCoordinate(latOrLong, coordinate):
+  isValid = False
+  for coordinateSet in coordinates:
+    if latOrLong == 'lat':
+      if coordinateSet["lat"][0] > coordinate > coordinateSet["lat"][1]:
+        isValid = True
+        break
+    else:
+      if coordinateSet["lng"][1] > coordinate > coordinateSet["lng"][0]:
+        isValid = True
+        break
 
-# LONG_WEST = -122.5087
-# LONG_EAST = -122.3937
-# LONG_NORTH = -122.40760
-# LONG_SOUTH = -122.501549
+  return isValid
+
+
+#Bay area coordinates:
+BORDERS = {
+  'NW': (37.95077976072001, -122.74698257446289),
+  'NE': (38.04601261075696, -122.07544326782227),
+  'SE': (37.16149648300589, -121.47668838500977),
+  'SW': (37.0421100532159, -122.42425918579102)
+}
 
 input_values = {}
 
-
 i = 0
+j = 0
 for price in prices:
   input_values["price"] = price
-  # print "-----------------", price
+
   for category in categories:
-    west = LATT_WEST
     input_values["category"] = category
-    # print "++++++++++++++++++++", category
 
-    while west > LATT_EAST:
-      north = LONG_NORTH
-      latitude = west
-      input_values["latitude"] = latitude
-      west = west - LATT_INCREMENT
-      while north > LONG_SOUTH:
-        longitude = north
-        input_values["longitude"] = longitude
+    latitude = BORDERS["NW"][0]
 
-        i = i + 1
-        # print i
-        # print north, west
-        area = RawDataHandler(input_values)
-        area.getRawJSON()
-        area.writeSearchTable()
-        area.writeBusinessTable()
-        area.writeReviewsTable()
+    while BORDERS["NW"][0] >= latitude >= BORDERS["SW"][0]:
+      longitude = BORDERS["NW"][1]
+      latValid = checkIfValidCoordinate('lat', latitude)
 
-        north = north - LONG_INCREMENT
+      newPoint = VincentyDistance(kilometers=0.353).destination(Point(latitude, longitude), 180)
+
+      if latValid:
+        input_values["latitude"] = latitude
+        latitude = newPoint.latitude
+      else:
+        latitude = newPoint.latitude
+        continue
+
+
+      while BORDERS["NE"][1] >= longitude >= BORDERS["NW"][1]:
+        j += 1
+        longValid = checkIfValidCoordinate('lng', longitude)
+        newPoint = VincentyDistance(kilometers=0.353).destination(Point(latitude, longitude), 90)
+        if longValid:
+          input_values["longitude"] = longitude
+          longitude = newPoint.longitude
+          i = i + 1
+          if 23000 < i < 28000:
+            time.sleep(0.1)
+            print i
+            area = RawDataHandler(input_values)
+            area.getRawJSON()
+            area.writeSearchTable()
+            area.writeBusinessTable()
+            area.writeReviewsTable()
+          elif i >= 28000:
+            print '---'
+            print i
+            sys.exit()
+        else:
+          longitude = newPoint.longitude
+          continue
+
